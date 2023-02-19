0f6b7dc1f817938daa0dc481e1ac7a5b43049f53
satya <satya143@gmail.com>
2023-02-19 22:54:19 +0530

    Added: Maximum Integer of 3 uses  generics

--- src/GenricsMaximum/Maximum.java
+++ src/GenricsMaximum/Maximum.java
@@ -1,7 +1,21 @@
 package GenricsMaximum;
 
 public class Maximum {
-    public static void main(String[] args) {
-        System.out.println("Maximum Problem using Generics : ");
+
+    public static <T extends Comparable<T>> T maximum(T number1, T number2, T number3) {
+        T maximum = number1;
+
+        if (number2.compareTo(maximum) > 0)
+            maximum = number2;
+
+        if (number3.compareTo(maximum) > 0)
+            maximum = number3;
+
+        return maximum;
+    }
+
+    public static void main(String args[]) {
+        System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+
     }
 }
