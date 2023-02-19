838412dbb1111186b75a32cd5a430021171d20c9
satya <satya143@gmail.com>
2023-02-19 23:10:52 +0530

    Added: Maximum String of 3 uses  generics

--- src/GenricsMaximum/Maximum.java
+++ src/GenricsMaximum/Maximum.java
@@ -15,8 +15,7 @@ public class Maximum {
     }
 
     public static void main(String args[]) {
-        System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
-                8.8, 7.7));//using float
-
+        System.out.printf("Maximum of %s, %s and %s is %s\n", "apple", "peach", "banana", maximum(
+                "apple", "peach", "banana"));//string
     }
 }
