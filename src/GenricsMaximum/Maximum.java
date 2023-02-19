2128fda74a2396201a606707faa81ac4cb14e9f5
satya <satya143@gmail.com>
2023-02-19 23:55:50 +0530

    Added:all above program doing with uses of generic class and variables

--- src/GenricsMaximum/Maximum.java
+++ src/GenricsMaximum/Maximum.java
@@ -1,21 +1,36 @@
 package GenricsMaximum;
 
-public class Maximum {
+public class Maximum<T extends Comparable<T>> {
+    T[] arr;
 
-    public static <T extends Comparable<T>> T maximum(T number1, T number2, T number3) {
-        T maximum = number1;
-
-        if (number2.compareTo(maximum) > 0)
-            maximum = number2;
-
-        if (number3.compareTo(maximum) > 0)
-            maximum = number3;
-
-        return maximum;
+    Maximum(T[] o) {
+        arr = o;
     }
 
-    public static void main(String args[]) {
-        System.out.printf("Maximum of %s, %s and %s is %s\n", "apple", "peach", "banana", maximum(
-                "apple", "peach", "banana"));//string
+    public T maximum() {
+        T value = arr[0];
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i].compareTo(value) > 0)
+                value = arr[i];
+        return value;
     }
 }
+
+class generics {
+    public static void main(String args[]) {
+        int i;
+        Integer numbers[] = {10, 2, 5, 4, 6, 1};
+        String str[] = {"apple", "peach", "banana"};
+        Float flot[] = {2.2f, 4.5f, 5.6f};
+        Maximum<Integer> integer = new Maximum<Integer>(numbers);
+        Maximum<String> string = new Maximum<String>(str);
+        Maximum<Float> floa = new Maximum<Float>(flot);
+        System.out.println("Max value in number: " + integer.maximum());
+
+        System.out.println("Max value in String : " + string.maximum());
+        System.out.println("Max value in double: " + floa.maximum());
+
+    }
+}
+
+
