eaf3ff880469dc13c6b7edb7174cecab3b25da33
satya <satya143@gmail.com>
2023-02-20 12:52:49 +0530

    Added:without Generic float maximum solution

--- src/WithoutGenricsMaximum/Maximum.java
+++ src/WithoutGenricsMaximum/Maximum.java
@@ -1,7 +1,7 @@
 package WithoutGenricsMaximum;
 
 public class Maximum {
-    public void maximum(int number1, int number2, int number3) {
+    public void maximum(float number1, float number2, float number3) {
         if (number1 >= number2 && number1 >= number3) {
             System.out.println(number1 + " is the maximum number.");
         } else if (number2 >= number1 && number2 >= number3) {
@@ -12,10 +12,10 @@ public class Maximum {
     }
 
     public static void main(String args[]) {
-        Maximum Integer = new Maximum();
+        Maximum Float = new Maximum();
         // call integer
-        Integer.maximum(4, 5, 8);
-        System.out.println(Integer);
+        Float.maximum(4.5f, 5.6f, 8.8f);
+        System.out.println(Float);
     }
 }
 
